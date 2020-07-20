6ce9387520c00af2bb74b4b2e3d40e000589471d
丁凯 <w_dingkai@belink.com>
2020-07-20 18:26:10 +0800

    单元测试

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Button from './button.vue'
 import Icon from './icon.vue'
 import ButtonGroup from './button-group.vue'
+import { expect } from 'chai'
 
 Vue.component('c-button', Button)
 Vue.component('c-icon', Icon)
@@ -13,3 +14,80 @@ new Vue({
     loading1: false
   }
 })
+
+//单元测试
+{
+  const Constructor = Vue.extend(Button)
+  const button = new Constructor({
+    propsData: {
+      icon: 'settings'
+    }
+  })
+  button.$mount('#test')
+  let useElement = button.$el.querySelector('use')
+  let href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#i-settings')
+}
+
+{
+  const Constructor = Vue.extend(Button)
+  const button = new Constructor({
+    propsData: {
+      icon: 'settings',
+      loading: true
+    }
+  })
+  button.$mount()
+  let useElement = button.$el.querySelector('use')
+  let href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#i-loading')
+}
+
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const button = new Constructor({
+    propsData: {
+      icon: 'settings'
+    }
+  })
+  button.$mount(div)
+  let svg = button.$el.querySelector('svg')
+  let { order } = window.getComputedStyle(svg)
+  expect(order).to.eq("1")
+}
+
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const button = new Constructor({
+    propsData: {
+      icon: 'settings',
+      iconPosition: 'right'
+    }
+  })
+  button.$mount(div)
+  let svg = button.$el.querySelector('svg')
+  let { order } = window.getComputedStyle(svg)
+  expect(order).to.eq("2")
+}
+
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const cButton = new Constructor({
+    propsData: {
+      icon: 'settings',
+      iconPosition: 'right'
+    }
+  })
+  cButton.$mount()
+  cButton.$on('click', function() {
+    
+  })
+  let button = cButton.$el
+  button.click()
+}
