d3aa6463eb4f74c156c6ceceb9ac8106a9eb4b1c
丁凯 <w_dingkai@belink.com>
2020-07-20 17:49:16 +0800

    添加button-group

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Button from './button.vue'
-import Icon from './icon'
+import Icon from './icon.vue'
+import ButtonGroup from './button-group.vue'
 
 Vue.component('c-button', Button)
 Vue.component('c-icon', Icon)
+Vue.component('c-button-group', ButtonGroup)
 
 new Vue({
   el: '#app',
---
+++ src/button-group.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="c-button-group">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    for (let node of this.$el.children) {
+      let name = node.nodeName.toLowerCase()
+      if (name !== 'button') {
+        console.warn(`g-button-group的子元素`)
+
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .c-button-group{
+    display: inline-flex;
+    vertical-align: middle;
+    > .c-button{
+      border-radius: 0;
+      &:not(:first-child) {
+        margin-left: -1px;
+      }
+      &:first-child{
+        border-top-left-radius: var(--border-radius);
+        border-bottom-left-radius: var(--border-radius);
+      }
+      &:last-child{
+        border-top-right-radius: var(--border-radius);
+        border-bottom-right-radius: var(--border-radius);
+      }
+      &:hover{
+        position: relative;
+        z-index: 1;
+      }
+    }
+  }
+</style>
