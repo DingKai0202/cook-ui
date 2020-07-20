3bb800eb883b42961b909088d5b63904cf73f7ae
丁凯 <w_dingkai@belink.com>
2020-07-20 15:44:58 +0800

    加载icon

--- src/button.vue
+++ src/button.vue
@@ -1,6 +1,7 @@
 <template>
   <button class="c-button" :class="{[`icon-${iconPosition}`]: true}">
     <c-icon v-if="icon" :name="icon"></c-icon>
+		<c-icon class="loading" name="loading"></c-icon>
 		<div class="content">
     	<slot></slot>
 		</div>
@@ -24,48 +25,55 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-    .c-button{
-        font-size: var(--font-size);
-        height: var(--button-height);
-        padding: 0 1em;    /*按钮没有设置宽度*/    
-        border-radius: var(--border-radius);
-        border: 1px solid var(--border-color);
-        background: var(--button-bg);
-				display: inline-flex;
-				justify-content: center;
-				align-items: center;
-				vertical-align: middle;
+  @keyframes spin{
+		0% { transform: rotate(0deg); }
+		100% { transform: rotate(360deg); }
+	}
+  .c-button{
+    font-size: var(--font-size);
+    height: var(--button-height);
+    padding: 0 1em;    /*按钮没有设置宽度*/    
+    border-radius: var(--border-radius);
+    border: 1px solid var(--border-color);
+    background: var(--button-bg);
+		display: inline-flex;
+		justify-content: center;
+		align-items: center;
+		vertical-align: middle;
 
-				&:hover{
-					border-color: var(--border-color-hover);
-				}
+		&:hover{
+			border-color: var(--border-color-hover);
+  	}
 		
-				&:hover{
-						border-color: var(--border-color-hover);
-				}
-				&:active{
-						background-color: var(--button-active-bg);
-				}
-				&:focus{
-						outline: none;
-				}
-				> .icon {
-					order: 1;
-					margin-right: .3em;
-				}
-				> .content{
-					order: 2;
-				}
-				&.icon-right{
-					> .icon{
-						order: 2;
-						margin-right: 0;
-						margin-left: .3em;
-					}
-					> .content{
-						order: 1;
-					}
-				}
-    }
+	  &:hover{
+			border-color: var(--border-color-hover);
+		}
+		&:active{
+			background-color: var(--button-active-bg);
+	  }
+		&:focus{
+			outline: none;
+		}
+		> .icon {
+			order: 1;
+			margin-right: .3em;
+		}
+		> .content{
+			order: 2;
+		}
+		&.icon-right{
+			> .icon{
+				order: 2;
+				margin-right: 0;
+				margin-left: .3em;
+			}
+			> .content{
+				order: 1;
+			}
+		}
+		.loading{
+			animation: spin 1.5s infinite linear;
+		}
+  }
 
 </style>
