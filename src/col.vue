a2872c85e07151aeae696c058b1321f3525a2049
丁凯 <w_dingkai@belink.com>
2020-09-13 22:57:53 +0800

    响应式基本完成

--- src/col.vue
+++ src/col.vue
@@ -84,7 +84,7 @@ export default {
         margin-left: ($n / 24) * 100%;
       }
     }
-    @media (min-width: 577px) and (max-width: 768px) {
+    @media (min-width: 577px) {
       $class-prefix: col-ipad-;
       @for $n from 1 through 24 {
         &.#{$class-prefix}#{$n} {
@@ -98,7 +98,7 @@ export default {
         }
       }
     }
-    @media (min-width: 769px) and (max-width: 992px) {
+    @media (min-width: 769px) {
       $class-prefix: col-narrow-pc-;
       @for $n from 1 through 24 {
         &.#{$class-prefix}#{$n} {
@@ -112,7 +112,7 @@ export default {
         }
       }
     }
-    @media (min-width: 993px) and (max-width: 1200px) {
+    @media (min-width: 993px) {
       $class-prefix: col-pc-;
       @for $n from 1 through 24 {
         &.#{$class-prefix}#{$n} {
